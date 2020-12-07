3521bdcacb938505ee16a61b00925c3f96f85bb8
Miulle <mika.strandb@gmail.com>
2020-12-07 22:39:04 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const videoElememt = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Promp the user to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+
+    } catch (error) {
+        // catch error here
+    }
+}
