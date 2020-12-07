b56b49168c3ce370d7bc2898e8bfa862423ff9c1
Miulle <mika.strandb@gmail.com>
2020-12-07 22:57:34 +0100

    finished picture in picture project

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const videoElememt = document.getElementById('video');
+const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Promp the user to select media stream, pass to video element, then play
 async function selectMediaStream() {
     try {
-
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     } catch (error) {
         // catch error here
     }
 }
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
